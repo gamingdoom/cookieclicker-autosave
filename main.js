ec25934b1ae5fb678f103794d64d72328dfa4e4d
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2021-11-23 18:46:35 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-var filename=Game.bakeryName.replace(/[^a-zA-Z0-9]+/g,'')+'Bakery'+Date.now();
-var text=Game.WriteSave(1);
-var blob=new Blob([text],{type:'text/plain;charset=utf-8'});
-saveAs(blob,filename+'.txt');
+const { resolve } = require("path/posix");
+
+function sleep(ms)
+{
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function save()
+{
+    while (true) 
+    {
+        await sleep(20000);
+        var filename=Game.bakeryName.replace(/[^a-zA-Z0-9]+/g,'')+'Bakery'+Date.now();
+        var text=Game.WriteSave(1);
+        var blob=new Blob([text],{type:'text/plain;charset=utf-8'});
+        saveAs(blob,filename+'.ccsave');
+    }
+}
+
+
 
