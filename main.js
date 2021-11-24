ace734cd3af0d5e0ccabf191d3d5847c4ecce885
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2021-11-23 18:38:25 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-//(function (saveNow) {
-    var filename=Game.bakeryName.replace(/[^a-zA-Z0-9]+/g,'')+'Bakery'+Date.now();
-    var text=Game.WriteSave(1);
-    var blob=new Blob([text],{type:'text/plain;charset=utf-8'});
-    saveAs(blob,filename+'.txt');
-//})();
+var filename=Game.bakeryName.replace(/[^a-zA-Z0-9]+/g,'')+'Bakery'+Date.now();
+var text=Game.WriteSave(1);
+var blob=new Blob([text],{type:'text/plain;charset=utf-8'});
+saveAs(blob,filename+'.txt');
+
