5f46f5cdbb2bbb721fe128e1159d87c2fb7276b5
gamingdoom <37276884+gamingdoom@users.noreply.github.com>
2021-11-23 18:35:16 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-(function () {
+(function(saveNow) {
     var filename=Game.bakeryName.replace(/[^a-zA-Z0-9]+/g,'')+'Bakery'+Date.now();
     var text=Game.WriteSave(1);
     var blob=new Blob([text],{type:'text/plain;charset=utf-8'});
